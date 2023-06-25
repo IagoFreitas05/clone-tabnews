08fa7156902963bb8adc02c180f767701803c3cd
Iago Freitas <60206884+IagoFreitas05@users.noreply.github.com>
2023-06-25 12:01:28 +0000

    - prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Clone for tabnews application developed in the curso.dev 
+
+Clone for tabnews application developed in the curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-  return <h1>Gabi, eu amo voce</h1>
+function Home() {
+  return <h1>Gabi, eu amo voce</h1>;
 }
 
 export default Home;
