aadd708c1c102dea93f8efb4b7dd5dc04789059c
Iago Freitas <60206884+IagoFreitas05@users.noreply.github.com>
2024-03-07 23:14:23 +0000

    - tabnews test and maxConnections for status page.

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,11 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(new Date(responseBody.updated_at).toISOString()).toEqual(
+    responseBody.updated_at
+  );
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
