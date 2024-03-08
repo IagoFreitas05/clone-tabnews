94760d909a79c27f2202fb97ce7af953b94c4c81
Iago Freitas <60206884+IagoFreitas05@users.noreply.github.com>
2024-03-08 20:01:39 +0000

    database.js try catch added

--- infra/database.js
+++ infra/database.js
@@ -9,8 +9,15 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+
   return result;
 }
 
