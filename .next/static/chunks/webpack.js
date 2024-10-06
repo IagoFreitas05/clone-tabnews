064e02a69fb6f0a5caa652875853b385e3a76ba1
Iago Freitas <60206884+IagoFreitas05@users.noreply.github.com>
2024-10-06 17:23:48 +0000

    adding migrations

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6286250441cdfec8"; }
+/******/ 		__webpack_require__.h = function() { return "3013b6ffff72fcc0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
