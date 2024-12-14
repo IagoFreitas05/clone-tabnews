b63e49ccc20a1606e62da20e5a15001b7980bc82
Iago Freitas <60206884+IagoFreitas05@users.noreply.github.com>
2024-12-14 11:52:19 +0000

    changed database.js

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -28,17 +28,15 @@ export default async function migrations(request, response) {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
       return response.status(200).json(pendingMigrations);
     }
-
     if (request.method === "POST") {
       const migratedMigrations = await migrationRunner({
         ...defaultMigrationOptions,
         dryRun: false,
       });
-
+      await dbClient.end();
       if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
       }
-
       return response.status(200).json(migratedMigrations);
     }
   } catch (error) {
