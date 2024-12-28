5ab5d830fbcdb3c6d60c2bce01afc07eea2428a5
Iago Freitas <60206884+IagoFreitas05@users.noreply.github.com>
2024-12-28 12:17:07 +0000

    adds wait-for-script

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,1 +1,16 @@
-console.log("📍 aguardando postgres aceitar conexões");
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n✅ Postgres está pronto e aceitando conexões");
+  }
+}
+process.stdout.write("\n\n📍 Aguardando postgres aceitar conexões");
+checkPostgres();
