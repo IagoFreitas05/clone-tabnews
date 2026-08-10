ad2db2895e553c71466f758351d6569866d87158
Iago Freitas <60206884+IagoFreitas05@users.noreply.github.com>
2026-08-10 09:55:58 +0000

    feat: run prettier

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,7 +10,7 @@ test("Get to /api/v1/status should return 200", async () => {
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
   expect(new Date(responseBody.updated_at).toISOString()).toEqual(
-    responseBody.updated_at
+    responseBody.updated_at,
   );
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
