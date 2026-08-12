dd8e036668ad2ef35f4c055aad6e9f6fc57ef9f8
Iago Freitas <60206884+IagoFreitas05@users.noreply.github.com>
2026-08-12 11:25:53 +0000

    feat(add status page to the project): add status page to the project

---
+++ pages/status/index.js
@@ -0,0 +1,54 @@
+import useSWR from "swr";
+
+async function fetchApi(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+function fetchData() {
+  const data = useSWR("api/v1/status", fetchApi, {
+    refreshInterval: 2000,
+  });
+  return data;
+}
+
+function UpdatedAt() {
+  const { data, isLoading } = fetchData();
+  if (!isLoading && data) {
+    return (
+      <p>
+        Last updated at: {new Date(data.updated_at).toLocaleString("pt-BR")}
+      </p>
+    );
+  }
+  return <p>Loading...</p>;
+}
+
+function DatabaseStatus() {
+  const { data, isLoading } = fetchData();
+  if (!isLoading && data) {
+    return (
+      <>
+        <pre>
+          <p>
+            Database connections:{" "}
+            {data.dependencies.database.oppened_connections}
+          </p>
+          <p>Database status: {data.dependencies.database.max_connections}</p>
+          <p>Database version: {data.dependencies.database.version}</p>
+        </pre>
+      </>
+    );
+  }
+}
+
+export default function statusPage() {
+  return (
+    <>
+      <h1>status page</h1>
+      <DatabaseStatus />
+      <UpdatedAt />
+    </>
+  );
+}
